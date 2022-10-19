da573be20983a6265f7cf415876c8e988d664849
Radu Elisei <phoenixcreates@yahoo.ro>
2022-10-19 17:14:50 +0300

    Add weapon model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,13 +6,11 @@ namespace dotnet_rpg.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-           
-        }
-        // cand vrei sa vezi o reprezentare a unui model in db folosesti DbSet
-         public DbSet<Character> Characters { get; set; }
-         public DbSet<User> Users { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        // cand vrei sa vezi o reprezentare a unui model in db folosesti DbSet
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
